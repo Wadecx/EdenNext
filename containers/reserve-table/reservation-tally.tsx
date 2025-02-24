9913ce947315c340b6ad44c29aef7a6252391df8
Wadecx <tyronmorcel@gmail.com>
2025-02-24 10:37:11 +0100

    Updates

--- containers/reserve-table/reservation-tally.tsx
+++ containers/reserve-table/reservation-tally.tsx
@@ -9,19 +9,21 @@ export const Reservation = () => {
         width={0}
         height={0}
         alt=""
-        className="md:w-full relative md:bottom-15 bottom-12 bg-cover"
+        className="md:w-full -mt-24"
         sizes="100vw"
       />
 
-      {/* Formulaire Tally */}
+      <div className="max-w-[80%] w-full">
         <iframe
           src="https://tally.so/embed/wz1N90?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1"
           loading="lazy"
           height="867"
+          width="100%"
           title="RÉSERVER VOTRE TABLE"
           className=""
         />
-
+      </div>
+      {/* Formulaire Tally */}
     </section>
   );
 };
